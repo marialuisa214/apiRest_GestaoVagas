5a9f81b6c917b2676d52f1ac5f451f4ade5b0888
Maria Luisa <maria.luisa.alves214@gmail.com>
2024-05-09 12:28:49 -0300

    :lock: adiciona criptografia as senha

---
+++ src/main/java/br/com/malualves/gestao_vagas/modules/empresa/useCase/AuthEmpresaUseCase.java
@@ -0,0 +1,8 @@
+package br.com.malualves.gestao_vagas.modules.empresa.useCase;
+
+import org.springframework.stereotype.Service;
+
+@Service // camada de autorizaçnao
+public class AuthEmpresaUseCase {
+
+}
--- src/main/java/br/com/malualves/gestao_vagas/modules/empresa/useCase/CreateEmpresaUseCase.java
+++ src/main/java/br/com/malualves/gestao_vagas/modules/empresa/useCase/CreateEmpresaUseCase.java
@@ -1,6 +1,7 @@
 package br.com.malualves.gestao_vagas.modules.empresa.useCase;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.malualves.gestao_vagas.exceptions.EmpresaFoundException;
@@ -12,11 +13,16 @@ public class CreateEmpresaUseCase {
     @Autowired
     private EmpresaRepositorio empresaRepositorio;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Empresa execute(Empresa novaEmpresa) {
         this.empresaRepositorio.findByUsernameOrEmail(novaEmpresa.getUsername(), novaEmpresa.getEmail())
                 .ifPresent((empr) -> {
                     throw new EmpresaFoundException();
                 });
+        var password = passwordEncoder.encode(novaEmpresa.getPassword());
+        novaEmpresa.setPassword(password);
 
         return this.empresaRepositorio.save(novaEmpresa);
 
